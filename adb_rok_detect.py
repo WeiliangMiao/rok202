ec49f66004e6e73eb0f8479868dcfd2d56de57a2
WeiliangMiao <78538241+WeiliangMiao@users.noreply.github.com>
2022-04-27 18:18:17 +0800

    Add files via upload

--- adb_rok_detect.py
+++ adb_rok_detect.py
@@ -20,31 +20,34 @@ def adb():
         time.sleep(1)
         if getKey() == 'down' or getKey() == 'other':
             os.system('adb shell input tap 164 978') #点击城市布局
+            time.sleep(2)
             os.system('adb shell input tap 1119 644') #点击斥候营地
             os.system('adb shell input tap 1231 865') #点击进入斥候营地
             os.system('adb shell input tap 1735 477') #点击第一个斥候
-            time.sleep(1)
+            time.sleep(2)
             os.system('adb shell input tap 1447 728') #点击地图探索
             os.system('adb shell input tap 1942 251') #点击第一个斥候出发
 
             os.system('adb shell input tap 164 978') #点击城市布局
+            time.sleep(2)
             os.system('adb shell input tap 1119 644') #点击斥候营地
             os.system('adb shell input tap 1231 865') #点击进入斥候营地
             os.system('adb shell input tap 1754 670') #点击第二个斥候
-            time.sleep(1)
+            time.sleep(2)
             os.system('adb shell input tap 1447 728') #点击地图探索
             os.system('adb shell input tap 1920 442') #点击第二个斥候出发
 
             os.system('adb shell input tap 164 978') #点击城市布局
+            time.sleep(2)
             os.system('adb shell input tap 1119 644') #点击斥候营地
             os.system('adb shell input tap 1231 865') #点击进入斥候营地
             os.system('adb shell input tap 1750 865') #点击第三个斥候
-            time.sleep(1)
+            time.sleep(2)
             os.system('adb shell input tap 1447 728') #点击地图探索
             os.system('adb shell input tap 1949 636') #点击第三个斥候出发
-            time.sleep(1)
+            time.sleep(2)
             
-            for _ in range(90):
+            for _ in range(80):
                 time.sleep(1)
                 if getKey() == 'up':
                     #os.system('adb shell input tap 960 120')#退出坐标UI
