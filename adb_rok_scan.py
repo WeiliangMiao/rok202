0262c45a4291a715016caba0f5454bea14a189c9
WeiliangMiao <78538241+WeiliangMiao@users.noreply.github.com>
2022-05-03 00:39:00 +0800

    Add files via upload

--- adb_rok_scan.py
+++ adb_rok_scan.py
@@ -1,7 +1,6 @@
 #adb_rok_scan
 #Redmi6 测试版，需要先手动进入界面
 import time
-import pyperclip
 import os
 import random
 
@@ -11,30 +10,33 @@ def click(x,y,r):
     os.system('adb shell input tap '+str(axes_x)+' '+str(axes_y))
     return
 
+os.system('adb devices')
+os.system('adb shell am startservice ca.zgrs.clipper/.ClipboardService')
+
 time.sleep(10)
 click(401,225,5)
 click(745,230,2)    #点。。。
 click(744,157,4)    #点复制昵称
-player_name=pyperclip.paste()
+player_name=os.popen('adb shell am broadcast -a clipper.get').read()[92:-2]
 click(1171,83,1)    #点关闭
 
 click(407,307,5)
 click(745,230,2)    #点。。。
 click(744,157,4)    #点复制昵称
-player_name=player_name+'\n'+pyperclip.paste()
+player_name=player_name+'\n'+os.popen('adb shell am broadcast -a clipper.get').read()[92:-2]
 click(1171,83,1)    #点关闭
 
 click(416,393,5)
 click(745,230,2)    #点。。。
 click(744,157,4)    #点复制昵称
-player_name=player_name+'\n'+pyperclip.paste()
+player_name=player_name+'\n'+os.popen('adb shell am broadcast -a clipper.get').read()[92:-2]
 click(1171,83,1)    #点关闭
 
 for _ in range(10):
     click(415,476,5)
     click(745,230,2)    #点。。。
     click(744,157,4)    #点复制昵称
-    player_name=player_name+'\n'+pyperclip.paste()
+    player_name=player_name+'\n'+os.popen('adb shell am broadcast -a clipper.get').read()[92:-2]
     click(1171,83,1)    #点关闭
 
 f = open('player_name.txt', "w", encoding="utf-8")
